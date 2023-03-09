ff6a87976d0b216c61754b6132715f0069ec3941
Kailydo <do422122@student.aisd.net>
2023-03-09 15:08:27 -0600

    flywheel and intake addition

--- src/main.py
+++ src/main.py
@@ -758,12 +758,18 @@ class Flywheel:
         self.motorGroup = MotorGroup(*[motors])
         self.flywheelPID = PID(Kp=1)
         self.endgameLaunched = False
+        self.velocity = 50
+
+        self.motorGroup.set_velocity(50, PERCENT)
 
     # TODO: add any other helper methods
 
     def toggleMotor(self):
         # TODO: add code to run/stop motor
-        pass
+        if self.motorGroup.is_spinning:
+            self.motorGroup.stop()
+        else:
+            self.motorGroup.spin(FORWARD)
 
     def launchEndgame(self):
         # TODO: add code to reverse flywheel to specific angle to launch endgame
@@ -772,7 +778,11 @@ class Flywheel:
 
     def changeSpeed(self):
         # TODO: add code to change motor speed low to high and low again
-        pass
+        if self.velocity == 100:
+            self.velocity=50
+        else:
+            self.velocity=100
+        self.motorGroup.set_velocity(self.velocity, PERCENT)
 
 
 class Indexer:
@@ -832,10 +842,12 @@ class Intake:
     # TODO: add any other helper methods
 
     def toggleMotor(self, direction=FORWARD):
+        self.motor.spin(FORWARD)
         # TODO: add code to run/stop motor
         pass
 
     def reverseMotor(self):
+        self.motor.spin(REVERSE)
         # TODO: add code to reverse motor in the event of jam
         pass
 
